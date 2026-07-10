4f4a281bac2973da1cd88c23276f4e37b79c1c26
jho <zchrx7625@naver.com>
2026-07-11 00:19:38 +0900

    fix:second commit

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -1,10 +1,9 @@
 import feedparser
-import git
 import os
 
 # 벨로그 RSS 피드 URL
 # example : rss_url = 'https://api.velog.io/rss/@soozi'
-rss_url = 'https://api.velog.io/rss/@[miy7625]'
+rss_url = 'https://api.velog.io/rss/@miy7625'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
@@ -16,13 +15,10 @@ posts_dir = os.path.join(repo_path, 'velog-posts')
 if not os.path.exists(posts_dir):
     os.makedirs(posts_dir)
 
-# 레포지토리 로드
-repo = git.Repo(repo_path)
-
 # RSS 피드 파싱
 feed = feedparser.parse(rss_url)
 
-# 각 글을 파일로 저장하고 커밋
+# 각 글을 파일로 저장
 for entry in feed.entries:
     # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
     file_name = entry.title
@@ -36,10 +32,3 @@ for entry in feed.entries:
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(entry.description)  # 글 내용을 파일에 작성
-
-        # 깃허브 커밋
-        repo.git.add(file_path)
-        repo.git.commit('-m', f'Add post: {entry.title}')
-
-# 변경 사항을 깃허브에 푸시
-repo.git.push()
